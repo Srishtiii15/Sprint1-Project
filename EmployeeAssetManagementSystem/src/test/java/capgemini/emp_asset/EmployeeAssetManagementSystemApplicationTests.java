e703ee02320b59a43271e9c50a6d92aeb9bb05e6
Srishtiii15 <88433266+Srishtiii15@users.noreply.github.com>
2021-08-31 14:41:29 +0530

    Add files via upload

---
+++ EmployeeAssetManagementSystem/src/test/java/capgemini/emp_asset/EmployeeAssetManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package capgemini.emp_asset;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeAssetManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
